0fb3830b476c477f66b6dcde1775a431f7ce0cf4
Liliya9307 <122142141+Liliya9307@users.noreply.github.com>
2024-07-20 00:10:39 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -1,23 +1,2 @@
-# profile.py
 
-import geni.portal as portal
-import geni.rspec.pg as rspec
-
-# Create a Request object to start building the RSpec.
-request = portal.context.makeRequestRSpec()
-
-# Node webserver
-webserver = request.RawPC("webserver")
-webserver.hardware_type = "pc3000"  # Specify hardware type if needed
-webserver.routable_control_ip = True
-
-# Node observer
-observer = request.RawPC("observer")
-observer.hardware_type = "pc3000"  # Specify hardware type if needed
-
-# Link between the nodes
-link = request.Link(members = [webserver, observer])
-
-# Print the generated RSpec
-portal.context.printRequestRSpec()
 
